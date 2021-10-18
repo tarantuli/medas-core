8cf7e64011659a60e121a470a85227475f6a090d
Patrick Morpey <info@morphp.nl>
2021-10-18 15:05:04 +0200

    Moved BaseException from medas-service-manager to here

---
+++ src/Exceptions/BaseException.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core\Exceptions;
+
+use Medas\Core\Str;
+
+abstract class BaseException extends \Exception
+{
+    private ?\Exception $previous = null;
+
+    public function __construct(...$arguments)
+    {
+        foreach ($arguments as &$argument) {
+            $argument = Str::fromVariable($argument)->truncateToCharLength(255);
+        }
+
+        $message = vsprintf($this->getPattern(), $arguments);
+
+        parent::__construct($message, 1, $this->previous);
+    }
+
+    abstract public function getPattern(): string;
+
+    public function setPrevious(?\Exception $previous): self
+    {
+        $this->previous = $previous;
+
+        return $this;
+    }
+}
