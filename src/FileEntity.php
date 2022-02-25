7649d46c6eed2928919c29c9ad38db9c71003290
Patrick Morpey <patrick@morphp.nl>
2022-02-25 10:51:10 +0100

    Added FileEntity interface

---
+++ src/FileEntity.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core;
+
+interface FileEntity
+{
+    public function id(): mixed;
+
+    public function setContent(string $content): self;
+
+    public function setName(?string $name): self;
+
+    public function name(): ?string;
+
+    public function mimetype(): string;
+
+    public function content(): string;
+
+    public function contentHash(): string;
+}
