9654a2358561af5b45c09d42b9be64364f326add
Patrick Morpey <patrick@morphp.nl>
2022-03-28 09:12:47 +0200

    Added ArrayCollection

---
+++ src/ArrayCollection.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core;
+
+class ArrayCollection
+{
+    private array $perIndex = [];
+    private array $indexes = [];
+
+    public function add(mixed $index, mixed $value): void
+    {
+        if (!array_key_exists($index, $this->perIndex)) {
+            $this->perIndex[$index] = [];
+            $this->indexes[] = $index;
+        }
+
+        $this->perIndex[$index][] = $value;
+    }
+
+    public function atIndex(mixed $index): array
+    {
+        return $this->perIndex[$index];
+    }
+
+    public function atMaxIndex(): array
+    {
+        return $this->perIndex[max($this->indexes)];
+    }
+
+    public function atMinIndex(): array
+    {
+        return $this->perIndex[min($this->indexes)];
+    }
+
+    public function atMaxCount(): array
+    {
+        $maxCount = null;
+        $atMaxCount = null;
+
+        foreach ($this->perIndex as $values) {
+            $count = count($values);
+            if ($maxCount === null || $count > $maxCount) {
+                $maxCount = $count;
+                $atMaxCount = $values;
+            }
+        }
+
+        return $atMaxCount;
+    }
+
+    public function atMinCount(): array
+    {
+        $minCount = null;
+        $atMinCount = null;
+
+        foreach ($this->perIndex as $values) {
+            $count = count($values);
+            if ($minCount === null || $count < $minCount) {
+                $minCount = $count;
+                $atMinCount = $values;
+            }
+        }
+
+        return $atMinCount;
+    }
+
+    public function indexes(): array
+    {
+        return $this->indexes;
+    }
+}
---
+++ tests/ArrayCollectionTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Test;
+
+use Medas\Core\ArrayCollection;
+use PHPUnit\Framework\TestCase;
+
+class ArrayCollectionTest extends TestCase
+{
+    public function testCreation(): ArrayCollection
+    {
+        $collection = new ArrayCollection();
+
+        $collection->add(1, 10);
+        $collection->add(2, 20);
+        $collection->add(2, 30);
+        $collection->add(3, 40);
+
+        self::assertInstanceOf(ArrayCollection::class, $collection);
+
+        return $collection;
+    }
+
+    /** @depends testCreation */
+    public function testAtIndex(ArrayCollection $collection): void
+    {
+        self::assertEquals([20, 30], $collection->atIndex(2));
+    }
+
+    /** @depends testCreation */
+    public function testAtMaxIndex(ArrayCollection $collection): void
+    {
+        self::assertEquals([40], $collection->atMaxIndex());
+    }
+
+    /** @depends testCreation */
+    public function testAtMinIndex(ArrayCollection $collection): void
+    {
+        self::assertEquals([10], $collection->atMinIndex());
+    }
+
+    /** @depends testCreation */
+    public function testAtMaxCount(ArrayCollection $collection): void
+    {
+        self::assertEquals([20, 30], $collection->atMaxCount());
+    }
+
+    /** @depends testCreation */
+    public function testAtMinCount(ArrayCollection $collection): void
+    {
+        self::assertEquals([10], $collection->atMinCount());
+    }
+
+    /** @depends testCreation */
+    public function testIndexes(ArrayCollection $collection): void
+    {
+        self::assertEquals([1, 2, 3], $collection->indexes());
+    }
+}
