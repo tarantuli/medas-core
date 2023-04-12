67765793c7e249dd5371ba94d976157adb18ee00
Patrick Morpey <patrick@morphp.nl>
2023-04-12 11:55:30 +0200

    Reorganized, moved many interfaces from service-manager to here

---
+++ src/Interfaces/Cache.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core\Interfaces;
+
+interface Cache
+{
+    public function get(string|array $key, callable $getter): mixed;
+
+    public function set(string|array $key, mixed $value): void;
+
+    public function remove(string|array $key): void;
+}
---
+++ src/Interfaces/Clearable.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core\Interfaces;
+
+interface Clearable
+{
+    public function clear(): void;
+}
