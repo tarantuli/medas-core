6e627604d6df25bc1bc76afed308e702adbb524c
Patrick Morpey <patrick@morphp.nl>
2024-07-17 12:03:24 +0200

    Added RectangleSides

---
+++ src/RectangleSides.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core;
+
+/**
+ * Represents the four sides of a rectangle
+ */
+class RectangleSides
+{
+    public float $top;
+    public float $right;
+    public float $bottom;
+    public float $left;
+
+    public function __construct(
+        float $top,
+        float $right = null,
+        float $bottom = null,
+        float $left = null,
+    )
+    {
+        $this->top = $top;
+        $this->right = $right === null ? $this->top : $right;
+        $this->bottom = $bottom === null ? $this->top : $bottom;
+        $this->left = $left === null ? $this->right : $left;
+    }
+}
