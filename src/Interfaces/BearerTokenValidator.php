40bb5f6db05e985c04401f93fc1e45138ece6736
Patrick Morpey <patrick@morphp.nl>
2024-07-25 14:47:05 +0200

    Added BearerTokenValidator

--- src/Interfaces/BearerTokenValidator.php
+++ src/Interfaces/BearerTokenValidator.php
@@ -6,9 +6,9 @@ namespace Medas\Core\Interfaces;
 
 /**
  * A Bearer token validator must take a token from an Authorization header value "Bearer <token>" and return the
- * associated user object if it's valid, null otherwise.
+ * associated user identifier if it's valid, null otherwise.
  */
 interface BearerTokenValidator
 {
-    public function user(string $token): object|null;
+    public function userId(string $token): string|null;
 }
