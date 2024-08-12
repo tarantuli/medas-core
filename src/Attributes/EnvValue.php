6172a69f5fde8c54847e9ad3c2e7e0284eb65353
Patrick Morpey <patrick@morphp.nl>
2024-08-12 12:15:12 +0200

    Added EnvValue attribute

---
+++ src/Attributes/EnvValue.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core\Attributes;
+
+/*
+ * Allow both to allow the attribute to be assigned to parameters in constructors with property promotion.
+ */
+#[\Attribute(\Attribute::TARGET_PARAMETER | \Attribute::TARGET_PROPERTY)]
+readonly class EnvValue
+{
+    public function __construct(
+        public string $name,
+    )
+    {
+    }
+}
