8bdd8763212de17753cac7c84839765920b4b1ac
Patrick Morpey <patrick@morphp.nl>
2025-02-11 10:54:22 +0100

    Moved properties to the constructor

--- src/File.php
+++ src/File.php
@@ -6,8 +6,12 @@ namespace Medas\Core;
 
 class File
 {
-    public string $content;
-    public string|null $name = null;
-    public string|null $mimetype = null;
-    public string|null $contentHash = null;
+    public function __construct(
+        public string      $content,
+        public string|null $name = null,
+        public string|null $mimetype = null,
+        public string|null $contentHash = null,
+    )
+    {
+    }
 }
