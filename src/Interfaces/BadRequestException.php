38eb1e94702f39d41ca02abc25ea39972b3b4c9f
Patrick Morpey <patrick@morphp.nl>
2026-02-02 09:58:22 +0100

    Added `BadRequestException` interface to mark exceptions indicating bad requests.

---
+++ src/Interfaces/BadRequestException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core\Interfaces;
+
+/**
+ * Exceptions marked with this interface indicate bad requests, instead of coding exceptions.
+ */
+interface BadRequestException
+{
+}
