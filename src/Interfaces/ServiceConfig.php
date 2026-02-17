cd37d0e0e02db6f2283b63ecef68b264937948ac
Patrick Morpey <patrick@morphp.nl>
2026-02-17 15:42:54 +0100

    Updated `ServiceConfig` interface to include methods for retrieving handlers and processors. Bumped `composer.json` version to 3.0.2.

--- src/Interfaces/ServiceConfig.php
+++ src/Interfaces/ServiceConfig.php
@@ -13,7 +13,16 @@ interface ServiceConfig
 {
     public function addExceptionHandler(ExceptionHandler $exceptionHandler): self;
 
+    /** @var ExceptionHandler[] $exceptionHandlers */
+    public function addExceptionHandlers(...$exceptionHandlers): self;
+
+    public function exceptionHandlers(): array;
+
     public function addParameterResolver(ParameterResolver $parameterResolver): self;
 
+    public function parameterResolvers(): array;
+
     public function addArgumentProcessor(ArgumentProcessor $argumentProcessor): self;
+
+    public function argumentProcessors(): array;
 }
