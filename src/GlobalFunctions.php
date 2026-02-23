2aeb9305580f06ca55ad01dbcbedf34671c081e6
Patrick Morpey <patrick@morphp.nl>
2026-02-23 11:33:58 +0100

    Guard against defining functions multiple times

--- src/GlobalFunctions.php
+++ src/GlobalFunctions.php
@@ -19,216 +19,218 @@ use Medas\Core\{
  * active @class(Medas\Core\ServiceManager) and @class(Medas\Core\ObjectInstantiator). Set ```$initialize``` to true
  * to reset the repository to a new instance.
  */
-function medas(bool $initialize = false): MedasRepository
-{
-    static $repository;
+if (!function_exists('medas')) {
+    function medas(bool $initialize = false): MedasRepository
+    {
+        static $repository;
 
-    if ($initialize || !isset($repository)) {
-        $repository = new MedasRepository();
+        if ($initialize || !isset($repository)) {
+            $repository = new MedasRepository();
+        }
+
+        return $repository;
     }
 
-    return $repository;
-}
-
-/**
- * This global function calls the @class(Medas\Core\Interfaces\ConfigManager) service and requests the config value for
- * the given value.
- */
-function config(string $path): mixed
-{
-    return medas()->serviceManager()
-        ->resolve(ConfigManager::class)
-        ->getValue($path);
-}
-
-/**
- * This global function returns the active @class(Medas\Core\Interfaces\ServiceManager) instance.
- */
-function sm(): ServiceManager
-{
-    return medas()->serviceManager();
-}
-
-/**
- * This global function calls the active @class(Medas\Core\Interfaces\ServiceManager) instance and asks it to resolve
- * $type to its service instance.
- *
- * The return value is an object of type `$type`.
- */
-/*
- * The return value is specified for PhpStorm using .phpstorm.meta.php
- */
-function service(string $type): object
-{
-    return medas()->serviceManager()
-        ->resolve($type);
-}
-
-/**
- * This global function turns an array of service objects into an array of class names.
- */
-function servicesToNames(array $services): array
-{
-    return array_map(static fn(object $service): string => $service::class, $services);
-}
-
-/**
- * This global function turns an array of class names into an array of service objects by calling service() on each name
- */
-function namesToServices(array $names): array
-{
-    return array_map(static fn(string $name): object => service($name), $names);
-}
-
-/**
- * This global function asks the given reflection object if it has an attribute that's of the given type. If so, it
- * returns an instance of the attribute class.
- *
- * The return value is null or an object of type `$type`.
- */
-/*
- * The return value is specified for PhpStorm using .phpstorm.meta.php
- */
-function attribute(
-    string                                                                                                    $type,
-    ReflectionClassConstant|ReflectionClass|ReflectionFunctionAbstract|ReflectionParameter|ReflectionProperty $reflector
-): object|null
-{
-    if (!$attributes = $reflector->getAttributes($type, ReflectionAttribute::IS_INSTANCEOF)) {
-        return null;
+    /**
+     * This global function calls the @class(Medas\Core\Interfaces\ConfigManager) service and requests the config value for
+     * the given value.
+     */
+    function config(string $path): mixed
+    {
+        return medas()->serviceManager()
+            ->resolve(ConfigManager::class)
+            ->getValue($path);
     }
 
-    return $attributes[0]->newInstance();
-}
-
-/**
- * This global function returns the value of the given property of the given object using reflection.
- */
-function propertyValue(object $object, string $propertyName): mixed
-{
-    return new ReflectionClass($object)->getProperty($propertyName)->getValue($object);
-}
-
-/*
- * Don't combine the next two functions or try to consolidate the content into one function and call it
- * from the function.
- *
- * The parameter signature should be tight, so users are not seduced to use the wrong method, leading
- * to unclear code (e.g., $parameterTypes = propertyTypes($parameter)).
- */
-/**
- * This method returns the types declared on the given ReflectionParameter as a normalized array
- * of ReflectionNamedType instances.
- *
- * @return ReflectionNamedType[]
- */
-function parameterTypes(ReflectionParameter $parameter): array
-{
-    return normalizeType($parameter->getType());
-}
-
-/**
- * This method returns the types declared on the given ReflectionProperty as a normalized
- * array of ReflectionNamedType instances.
- *
- * @return ReflectionNamedType[]
- */
-function propertyTypes(ReflectionProperty $parameter): array
-{
-    return normalizeType($parameter->getType());
-}
-
-function normalizeType(ReflectionIntersectionType|ReflectionNamedType|ReflectionUnionType|null $type): array
-{
-    if ($type === null) {
-        return [];
+    /**
+     * This global function returns the active @class(Medas\Core\Interfaces\ServiceManager) instance.
+     */
+    function sm(): ServiceManager
+    {
+        return medas()->serviceManager();
     }
 
-    return ($type instanceof ReflectionUnionType || $type instanceof ReflectionIntersectionType)
-        ? $type->getTypes()
-        : [$type];
-}
-
-function whileTrue(callable $callable, int $maxCount = 256): void
-{
-    $counter = 0;
-
-    while ($callable() && ++$counter < $maxCount) {
-        // Do nothing else
+    /**
+     * This global function calls the active @class(Medas\Core\Interfaces\ServiceManager) instance and asks it to resolve
+     * $type to its service instance.
+     *
+     * The return value is an object of type `$type`.
+     */
+    /*
+     * The return value is specified for PhpStorm using .phpstorm.meta.php
+     */
+    function service(string $type): object
+    {
+        return medas()->serviceManager()
+            ->resolve($type);
     }
-}
 
-function is_nihil(float $value): bool
-{
-    return FloatingNumber::SMALL_NEGATIVE < $value && $value < FloatingNumber::SMALL_POSITIVE;
-}
-
-/**
- * The return value is an object of type `$class`.
- *
- * Prefer to use a ServiceManager implementation or apply the AsSingleton trait to get a singleton instance of a class.
- *
- * This function should be used when you want to have singleton instances of readonly data objects, which don't fit
- * service characteristics, nor cannot be used with AsSingleton (due to a readonly nature).
- */
-/*
- * The return value is specified for PhpStorm using .phpstorm.meta.php
- */
-function singleton(string $class): object
-{
-    return SingletonStore::get($class);
-}
-
-/**
- * This function resolves the EventDispatcher and then dispatches the event
- */
-/*
- * The return value is specified for PhpStorm using .phpstorm.meta.php
- */
-function dispatch(object $event): object
-{
-    return service(EventDispatcher::class)->dispatch($event);
-}
-
-/**
- * This function resolves the EventDispatcher and then dispatches the vote object. If allowedAccess is not true, it
- * throws the exception, otherwise it does nothing.
- */
-function allowElseThrow(BasicVote $vote, Throwable|callable $exception): void
-{
-    dispatch($vote);
-
-    if (!$vote->allowedAccess) {
-        throw ($exception instanceof Throwable ? $exception : $exception());
+    /**
+     * This global function turns an array of service objects into an array of class names.
+     */
+    function servicesToNames(array $services): array
+    {
+        return array_map(static fn(object $service): string => $service::class, $services);
     }
-}
 
-/**
- * This function resolves the CacheManager and then fetches the results of the given key
- *
- * @param string|string[] $key
- */
-function cache(string|array $key, callable $getter, string|null $cache = null): mixed
-{
-    return service(CacheManager::class)->get($cache)->get($key, $getter);
-}
+    /**
+     * This global function turns an array of class names into an array of service objects by calling service() on each name
+     */
+    function namesToServices(array $names): array
+    {
+        return array_map(static fn(string $name): object => service($name), $names);
+    }
 
-/**
- * This function resolves the CacheManager and then sets the given value
- *
- * @param string|string[] $key
- */
-function cacheSet(string|array $key, mixed $value, string|null $cache = null): void
-{
-    service(CacheManager::class)->get($cache)->set($key, $value);
-}
+    /**
+     * This global function asks the given reflection object if it has an attribute that's of the given type. If so, it
+     * returns an instance of the attribute class.
+     *
+     * The return value is null or an object of type `$type`.
+     */
+    /*
+     * The return value is specified for PhpStorm using .phpstorm.meta.php
+     */
+    function attribute(
+        string                                                                                                    $type,
+        ReflectionClassConstant|ReflectionClass|ReflectionFunctionAbstract|ReflectionParameter|ReflectionProperty $reflector
+    ): object|null
+    {
+        if (!$attributes = $reflector->getAttributes($type, ReflectionAttribute::IS_INSTANCEOF)) {
+            return null;
+        }
 
-/**
- * This function resolves the CacheManager and then removes the value of the given key
- *
- * @param string|string[] $key
- */
-function cacheUnset(string|array $key, string|null $cache = null): void
-{
-    service(CacheManager::class)->get($cache)->remove($key);
+        return $attributes[0]->newInstance();
+    }
+
+    /**
+     * This global function returns the value of the given property of the given object using reflection.
+     */
+    function propertyValue(object $object, string $propertyName): mixed
+    {
+        return new ReflectionClass($object)->getProperty($propertyName)->getValue($object);
+    }
+
+    /*
+     * Don't combine the next two functions or try to consolidate the content into one function and call it
+     * from the function.
+     *
+     * The parameter signature should be tight, so users are not seduced to use the wrong method, leading
+     * to unclear code (e.g., $parameterTypes = propertyTypes($parameter)).
+     */
+    /**
+     * This method returns the types declared on the given ReflectionParameter as a normalized array
+     * of ReflectionNamedType instances.
+     *
+     * @return ReflectionNamedType[]
+     */
+    function parameterTypes(ReflectionParameter $parameter): array
+    {
+        return normalizeType($parameter->getType());
+    }
+
+    /**
+     * This method returns the types declared on the given ReflectionProperty as a normalized
+     * array of ReflectionNamedType instances.
+     *
+     * @return ReflectionNamedType[]
+     */
+    function propertyTypes(ReflectionProperty $parameter): array
+    {
+        return normalizeType($parameter->getType());
+    }
+
+    function normalizeType(ReflectionIntersectionType|ReflectionNamedType|ReflectionUnionType|null $type): array
+    {
+        if ($type === null) {
+            return [];
+        }
+
+        return ($type instanceof ReflectionUnionType || $type instanceof ReflectionIntersectionType)
+            ? $type->getTypes()
+            : [$type];
+    }
+
+    function whileTrue(callable $callable, int $maxCount = 256): void
+    {
+        $counter = 0;
+
+        while ($callable() && ++$counter < $maxCount) {
+            // Do nothing else
+        }
+    }
+
+    function is_nihil(float $value): bool
+    {
+        return FloatingNumber::SMALL_NEGATIVE < $value && $value < FloatingNumber::SMALL_POSITIVE;
+    }
+
+    /**
+     * The return value is an object of type `$class`.
+     *
+     * Prefer to use a ServiceManager implementation or apply the AsSingleton trait to get a singleton instance of a class.
+     *
+     * This function should be used when you want to have singleton instances of readonly data objects, which don't fit
+     * service characteristics, nor cannot be used with AsSingleton (due to a readonly nature).
+     */
+    /*
+     * The return value is specified for PhpStorm using .phpstorm.meta.php
+     */
+    function singleton(string $class): object
+    {
+        return SingletonStore::get($class);
+    }
+
+    /**
+     * This function resolves the EventDispatcher and then dispatches the event
+     */
+    /*
+     * The return value is specified for PhpStorm using .phpstorm.meta.php
+     */
+    function dispatch(object $event): object
+    {
+        return service(EventDispatcher::class)->dispatch($event);
+    }
+
+    /**
+     * This function resolves the EventDispatcher and then dispatches the vote object. If allowedAccess is not true, it
+     * throws the exception, otherwise it does nothing.
+     */
+    function allowElseThrow(BasicVote $vote, Throwable|callable $exception): void
+    {
+        dispatch($vote);
+
+        if (!$vote->allowedAccess) {
+            throw ($exception instanceof Throwable ? $exception : $exception());
+        }
+    }
+
+    /**
+     * This function resolves the CacheManager and then fetches the results of the given key
+     *
+     * @param string|string[] $key
+     */
+    function cache(string|array $key, callable $getter, string|null $cache = null): mixed
+    {
+        return service(CacheManager::class)->get($cache)->get($key, $getter);
+    }
+
+    /**
+     * This function resolves the CacheManager and then sets the given value
+     *
+     * @param string|string[] $key
+     */
+    function cacheSet(string|array $key, mixed $value, string|null $cache = null): void
+    {
+        service(CacheManager::class)->get($cache)->set($key, $value);
+    }
+
+    /**
+     * This function resolves the CacheManager and then removes the value of the given key
+     *
+     * @param string|string[] $key
+     */
+    function cacheUnset(string|array $key, string|null $cache = null): void
+    {
+        service(CacheManager::class)->get($cache)->remove($key);
+    }
 }
