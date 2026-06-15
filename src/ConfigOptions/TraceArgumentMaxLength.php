31d2d42a125c52f28745848b5542bdd3476dbfdb
Patrick Morpey <patrick@morphp.nl>
2026-06-15 16:50:56 +0200

    Added `TraceFormatter` to enhance exception trace formatting and introduced `TraceArgumentMaxLength` configuration option.

---
+++ src/ConfigOptions/TraceArgumentMaxLength.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Core\ConfigOptions;
+
+use Medas\Core\{Attributes\Service, Interfaces\ConfigGroup, Interfaces\ConfigOption};
+
+#[Service]
+readonly class TraceArgumentMaxLength implements ConfigOption
+{
+    public function __construct(
+        private CoreGroup $group,
+    )
+    {
+    }
+
+    public function group(): ConfigGroup
+    {
+        return $this->group;
+    }
+
+    public function name(): string
+    {
+        return 'trace-argument-max-length';
+    }
+
+    public function description(): string
+    {
+        return 'Maximum number of characters for string arguments printed in stack traces';
+    }
+
+    public function hasDefault(): bool
+    {
+        return true;
+    }
+
+    public function default(): int
+    {
+        return 156;
+    }
+}
