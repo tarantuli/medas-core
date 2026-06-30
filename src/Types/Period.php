61cd6597df3e56f8232af1d28341d3a641406362
Patrick Morpey <patrick@morphp.nl>
2026-06-30 10:34:09 +0200

    Updated `Period` type to extend `Text` instead of `Binary`.

--- src/Types/Period.php
+++ src/Types/Period.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Medas\Core\Types;
 
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
-class Period extends Binary
+class Period extends Text
 {
     public function __construct()
     {
