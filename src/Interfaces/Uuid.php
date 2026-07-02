ab66f1e7d8e7bb752a3f1fca04329e7530c01c20
Patrick Morpey <patrick@morphp.nl>
2026-07-02 12:23:44 +0200

    Added `JsonSerializable` to `Uuid` interface for JSON support.

--- src/Interfaces/Uuid.php
+++ src/Interfaces/Uuid.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Medas\Core\Interfaces;
 
 /**
- * A Uuid object should be castable to a readable string. ```toBytes()``` should return a byte representation string for
- * storage purposes.
+ * A Uuid object should be castable to a readable string. ```toBytes()``` should return a byte representation string
+ * for storage purposes.
  */
-interface Uuid extends \Stringable
+interface Uuid extends \Stringable, \JsonSerializable
 {
     public function toBytes(): string;
 }
