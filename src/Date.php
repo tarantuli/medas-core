bd91dea5377540aab5efc821ec35fdc17551affc
Patrick Morpey <patrick@morphp.nl>
2026-07-09 12:44:44 +0200

    Added `today` static constructor to `Date` class for obtaining the current date.

--- src/Date.php
+++ src/Date.php
@@ -6,6 +6,17 @@ namespace Medas\Core;
 
 readonly class Date
 {
+    public static function today(): self
+    {
+        $now = new \DateTimeImmutable();
+
+        return new self(
+            (int) $now->format('Y'),
+            (int) $now->format('m'),
+            (int) $now->format('d'),
+        );
+    }
+
     public function __construct(
         public int $year,
         public int $month,
