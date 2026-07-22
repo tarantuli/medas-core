9dd01952591230a822aaebdcd8c9ff1e644ad586
Patrick Morpey <patrick@morphp.nl>
2026-07-22 10:42:50 +0200

    Enhanced `ServiceNotFoundByType` exception to include active resolves in message.

--- src/Exceptions/ServiceNotFoundByType.php
+++ src/Exceptions/ServiceNotFoundByType.php
@@ -6,13 +6,13 @@ namespace Medas\Core\Exceptions;
 
 class ServiceNotFoundByType extends BaseException
 {
-    public function __construct(string $type)
+    public function __construct(string $type, array $activeResolves)
     {
-        parent::__construct($type);
+        parent::__construct($type, implode(', ', $activeResolves));
     }
 
     public function pattern(): string
     {
-        return 'service not found with type %s';
+        return 'service not found with type %s, active resolves: %s';
     }
 }
